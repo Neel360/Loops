286666ea6b19ac4d1eaaa9e9e6455b9495abff49
neel-guckool <neel001g@gmail.com>
2026-05-13 16:37:23 +0100

    first commit

---
+++ excersice_1.py
@@ -0,0 +1,7 @@
+n = input("Enter a number:")
+n = int(n)
+sum = 0
+
+for i in range(n+1):
+    sum = sum + i
+    print("The sum is:", sum)
---
+++ exercise_3.py
@@ -0,0 +1,7 @@
+n = input("Enter a number: ")
+n = int(n)
+
+print("The numbers from {0} to {1} are: ".format(1, n))
+
+for i in range(n, 0, -1):
+    print(i)
